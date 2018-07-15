097e77d6eb322b1763923d68b24763177acc52a9
Siddharth Mehta <siddharthmehta@Siddharths-MacBook-Pro.local>
2018-07-15 18:58:41 +0530

    Greet New User, and Alert

--- server/server.js
+++ server/server.js
@@ -15,8 +15,21 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
     console.log('User Connected.');
 
+    socket.emit('newMessage', {
+        from: 'Admin',
+        text: 'Welcome to the Chat App!',
+        createdAt: new Date().getTime()
+    });
+
+    socket.broadcast.emit('newMessage', {
+        from: 'Admin',
+        text: 'New User Joined',
+        createdAt: new Date().getTime()
+    });
+
     socket.on('createMessage', (message) => {
         console.log('createMessage', message);
+
         io.emit('newMessage', {
             from: message.from,
             text: message.text,
