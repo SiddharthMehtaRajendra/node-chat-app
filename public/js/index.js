369d0022e5504d7dcb2fa9e23f315440def83e0e
Siddharth Mehta <siddharthmehta@Siddharths-MacBook-Pro.local>
2018-07-15 21:14:50 +0530

    Fixes

--- public/js/index.js
+++ public/js/index.js
@@ -17,4 +17,19 @@ socket.emit('createMessage', {
 
 socket.on('newMessage', function (message){
     console.log('New Message', message);
+    var li = $('<li></li>');
+    li.text(`${message.from}: ${message.text}`);
+
+    $("#messages").append(li);
+});
+
+$('#message-form').on('submit', function(e) {
+    e.preventDefault();
+
+    socket.emit('createMessage', {
+        from: 'User',
+        text: $('#msgId').val()
+    }, function(){
+
+    });
 });
